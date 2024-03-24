5bd15b8950fd058c9df2110d6e7673847f6f36f8
Denis Deniz <denis2.glez@gmail.com>
2024-03-24 17:54:57 +0100

    feat(cli): add CLI with operation mode, IP and port options.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello World!");
+use std::net::IpAddr;
+
+use clap::Parser;
+use omni_node::{client, server};
+use serde::Serialize;
+
+#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
+#[serde(rename_all = "snake_case")]
+enum Mode {
+    /// Mode of operation as client (default).
+    #[default]
+    Client,
+    /// Mode of operation as server.
+    Server,
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Operation mode.
+    #[arg(short, long, value_enum)]
+    mode: Option<Mode>,
+
+    /// Server IP address.
+    #[arg(short, long, default_value = "127.0.0.1")]
+    ip_addr_server: Option<IpAddr>,
+
+    /// Server TCP port.
+    #[arg(short, long, default_value = "9696")]
+    port_server: Option<u16>,
+}
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.mode {
+        Some(Mode::Server) => {
+            server::start(cli.ip_addr_server, cli.port_server).await?;
+        }
+        _ => {
+            client::start(cli.ip_addr_server, cli.port_server).await?;
+        }
+    }
+
+    Ok(())
 }
